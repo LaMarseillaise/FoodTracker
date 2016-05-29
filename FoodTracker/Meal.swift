95281f0428d7374275b6526c31902d8f61cba0ec
Andur Carr <carr.andur@gmail.com>
2016-05-28 17:00:55 -0700

    Finish Define Your Data Model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,28 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Andur Carr on 5/28/16.
+//
+//
+
+import UIKit
+
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int){
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
